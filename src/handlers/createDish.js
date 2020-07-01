ba3ec2667b6124f428b06497765e0f406a59c995
Nataliya Pogodina <n.pogodina@gmail.com>
2020-07-01 15:13:06 -0700

    Added put unto DynamoDB to createDish

--- src/handlers/createDish.js
+++ src/handlers/createDish.js
@@ -1,12 +1,29 @@
+import { v4 as uuid } from "uuid";
+import AWS from "aws-sdk";
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
 async function createDish(event, context) {
   const { name } = JSON.parse(event.body);
   const now = new Date();
 
   const dish = {
+    userId: "1",
     name,
-    breakfast: "yes",
-    createdAt: now.toISOString(),
+    dishId: uuid(),
+    timestamp: now.toISOString(),
+    breakfast: "no",
+    lunch: "yes",
+    dinner: "yes",
+    other: "no",
   };
+
+  await dynamodb
+    .put({
+      TableName: "DishesTable",
+      Item: dish,
+    })
+    .promise();
 
   return {
     statusCode: 201,
