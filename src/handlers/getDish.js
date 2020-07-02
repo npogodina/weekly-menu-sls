ef7e5dbd88c5c8f3d4dfa43e6d3628c39d185b06
Nataliya Pogodina <n.pogodina@gmail.com>
2020-07-02 08:47:36 -0700

    Wrote core code for getDish function

---
+++ src/handlers/getDish.js
@@ -0,0 +1,44 @@
+import AWS from "aws-sdk";
+import middy from "@middy/core";
+import httpJsonBodyParser from "@middy/http-json-body-parser";
+import httpEventNormalizer from "@middy/http-event-normalizer";
+import httpErrorHandler from "@middy/http-error-handler";
+import createError from "http-errors";
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+async function getDish(event, context) {
+  let dish;
+  const { id, name } = event.pathParameters;
+
+  try {
+    const result = await dynamodb
+      .get({
+        TableName: process.env.DISHES_TABLE_NAME,
+        Key: {
+          userId: id,
+          name: name,
+        },
+      })
+      .promise();
+
+    dish = result.Item;
+  } catch (error) {
+    console.error(error);
+    throw new createError.InternalServerError(error);
+  }
+
+  if (!dish) {
+    throw new createError.NotFound(`Dish not found`);
+  }
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify(dish),
+  };
+}
+
+export const handler = middy(getDish)
+  .use(httpJsonBodyParser())
+  .use(httpEventNormalizer())
+  .use(httpErrorHandler());
