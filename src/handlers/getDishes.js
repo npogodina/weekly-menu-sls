a2151a177af2dc0d867120fd96720d9b5f23eba7
Nataliya Pogodina <n.pogodina@gmail.com>
2020-07-02 06:15:15 -0700

    Added getDishes function

---
+++ src/handlers/getDishes.js
@@ -0,0 +1,35 @@
+import AWS from "aws-sdk";
+import middy from "@middy/core";
+import httpJsonBodyParser from "@middy/http-json-body-parser";
+import httpEventNormalizer from "@middy/http-event-normalizer";
+import httpErrorHandler from "@middy/http-error-handler";
+import createError from "http-errors";
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+async function getDishes(event, context) {
+  let dishes;
+
+  try {
+    const result = await dynamodb
+      .scan({
+        TableName: process.env.DISHES_TABLE_NAME,
+      })
+      .promise();
+
+    dishes = result.Items;
+  } catch (error) {
+    console.error(error);
+    throw new createError.InternalServerError(error);
+  }
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify(dish),
+  };
+}
+
+export const handler = middy(getDishes)
+  .use(httpJsonBodyParser())
+  .use(httpEventNormalizer())
+  .use(httpErrorHandler());
