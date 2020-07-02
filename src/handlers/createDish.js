02c677aa1784d2b9324bf004b71dbb0b8bdb3fc5
Nataliya Pogodina <n.pogodina@gmail.com>
2020-07-01 21:23:45 -0700

    Added middy and http-errors libraries

--- src/handlers/createDish.js
+++ src/handlers/createDish.js
@@ -1,5 +1,9 @@
 import { v4 as uuid } from "uuid";
 import AWS from "aws-sdk";
+import middy from "@middy/core";
+import httpJsonBodyParser from "@middy/http-json-body-parser";
+import httpEventNormalizer from "@middy/http-event-normalizer";
+import httpErrorHandler from "@middy/http-error-handler";
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
