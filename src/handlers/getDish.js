9ce51e81ba18ee7301098736ff1f9290acec9562
Nataliya Pogodina <n.pogodina@gmail.com>
2020-07-02 08:55:46 -0700

    Added url-slug to handle name parameter

--- src/handlers/getDish.js
+++ src/handlers/getDish.js
@@ -4,12 +4,14 @@ import httpJsonBodyParser from "@middy/http-json-body-parser";
 import httpEventNormalizer from "@middy/http-event-normalizer";
 import httpErrorHandler from "@middy/http-error-handler";
 import createError from "http-errors";
+import urlSlug from "url-slug";
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 async function getDish(event, context) {
   let dish;
-  const { id, name } = event.pathParameters;
+  let { id, name } = event.pathParameters;
+  name = urlSlug.revert(name, "-", urlSlug.transformers.titlecase);
 
   try {
     const result = await dynamodb
