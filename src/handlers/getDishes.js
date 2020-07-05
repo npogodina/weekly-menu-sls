a6fa40bfb1dd9c6b4349abe9b942e9b2afef88dc
Nataliya Pogodina <n.pogodina@gmail.com>
2020-07-05 10:33:25 -0700

    Changed getDishes to use query instead of scan

--- src/handlers/getDishes.js
+++ src/handlers/getDishes.js
@@ -5,20 +5,47 @@ import commonMiddleware from "../lib/commonMiddleware";
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 async function getDishes(event, context) {
+  // const { userId } = event.body;
+
   let dishes;
 
-  try {
-    const result = await dynamodb
-      .scan({
-        TableName: process.env.DISHES_TABLE_NAME,
-      })
-      .promise();
+  const nataliya = "google-oauth2|102165070264738113845";
+  const params = {
+    TableName: process.env.DISHES_TABLE_NAME,
+    // IndexName: 'Index',
+    KeyConditionExpression: "userId = :hkey",
+    ExpressionAttributeValues: {
+      ":hkey": nataliya,
+    },
+  };
 
+  try {
+    const result = await dynamodb.query(params).promise();
     dishes = result.Items;
   } catch (error) {
     console.error(error);
     throw new createError.InternalServerError(error);
   }
+
+  // dynamodb.query(params, function (err, data) {
+  //   if (err) {
+  //     console.error(
+  //       "Unable to read item. Error JSON:",
+  //       JSON.stringify(err, null, 2)
+  //     );
+  //   } else {
+  //     dishes = data;
+  //   }
+  // });
+
+  // try {
+  //   const result = await dynamodb.query(params).promise();
+
+  //   dishes = result.Items;
+  // } catch (error) {
+  //   console.error(error);
+  //   throw new createError.InternalServerError(error);
+  // }
 
   return {
     statusCode: 200,
