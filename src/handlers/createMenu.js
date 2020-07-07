7076f98d7676329bbf2fc2303532294581a6b341
Nataliya Pogodina <n.pogodina@gmail.com>
2020-07-07 05:47:21 -0700

    Added dishes query by userID to createMenu

--- src/handlers/createMenu.js
+++ src/handlers/createMenu.js
@@ -5,32 +5,51 @@ import commonMiddleware from "../lib/commonMiddleware";
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 async function createMenu(event, context) {
-  const { userId, startDate } = event.body;
+  // const { userId, startDate } = event.body;
+  const { userId } = event.body;
+
   // const { sub } = event.requestContext.authorizer;
 
-  const now = new Date();
+  // const now = new Date();
 
-  const menu = {
-    userId,
-    startDate,
-    timestamp: now.toISOString(),
+  let dishes;
+  const params = {
+    TableName: process.env.DISHES_TABLE_NAME,
+    KeyConditionExpression: "userId = :hkey",
+    ExpressionAttributeValues: {
+      ":hkey": userId,
+    },
   };
 
   try {
-    await dynamodb
-      .put({
-        TableName: process.env.MENUS_TABLE_NAME,
-        Item: menu,
-      })
-      .promise();
+    const result = await dynamodb.query(params).promise();
+    dishes = JSON.stringify(result.Items);
   } catch (error) {
     console.error(error);
     throw new createError.InternalServerError(error);
   }
 
+  // const menu = {
+  //   userId,
+  //   startDate,
+  //   timestamp: now.toISOString(),
+  // };
+
+  // try {
+  //   await dynamodb
+  //     .put({
+  //       TableName: process.env.MENUS_TABLE_NAME,
+  //       Item: menu,
+  //     })
+  //     .promise();
+  // } catch (error) {
+  //   console.error(error);
+  //   throw new createError.InternalServerError(error);
+  // }
+
   return {
     statusCode: 201,
-    body: JSON.stringify(menu),
+    body: dishes,
   };
 }
 
