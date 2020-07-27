a5b12e3307543a39851d2a5a877d3ee059b07d59
Nataliya Pogodina <n.pogodina@gmail.com>
2020-07-27 07:17:28 -0700

    Modified deleteDish

--- src/handlers/deleteDish.js
+++ src/handlers/deleteDish.js
@@ -1,48 +1,19 @@
 import AWS from "aws-sdk";
 import createError from "http-errors";
-// import urlSlug from "url-slug";
 import commonMiddleware from "../lib/commonMiddleware";
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 async function deleteDish(event, context) {
-  let dish;
   console.log(event.pathParameters);
   let { id } = event.pathParameters;
-  // name = urlSlug.revert(name, "-", urlSlug.transformers.titlecase);
-
-  const params = {
-    TableName: process.env.DISHES_TABLE_NAME,
-    IndexName: "dishIdGlobalIndex",
-    KeyConditionExpression: "dishId = :hkey",
-    ExpressionAttributeValues: {
-      ":hkey": id,
-    },
-  };
-
-  try {
-    const result = await dynamodb.query(params).promise();
-    console.log(result);
-    dish = result.Items[0];
-  } catch (error) {
-    console.error(error);
-    throw new createError.InternalServerError(error);
-  }
-
-  if (!dish) {
-    throw new createError.NotFound(`Dish not found`);
-  }
-
-  const userId = dish.userId;
-  const name = dish.name;
 
   try {
     const result = await dynamodb
       .delete({
-        TableName: process.env.DISHES_TABLE_NAME,
+        TableName: process.env.RECIPES_TABLE_NAME,
         Key: {
-          userId: userId,
-          name: name,
+          dishId: id,
         },
       })
       .promise();
