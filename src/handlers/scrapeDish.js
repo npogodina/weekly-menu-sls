8342561cfc61986b6e78514da5dd1ca344473b5b
Nataliya Pogodina <n.pogodina@gmail.com>
2020-07-28 14:52:18 -0700

    Added dynamodb.put to scrapeDish func

--- src/handlers/scrapeDish.js
+++ src/handlers/scrapeDish.js
@@ -1,61 +1,46 @@
-// import { v4 as uuid } from "uuid";
-// import AWS from "aws-sdk";
-// import createError from "http-errors";
+import { v4 as uuid } from "uuid";
+import AWS from "aws-sdk";
+import createError from "http-errors";
 import commonMiddleware from "../lib/commonMiddleware";
 import recipeScraper from "recipe-scraper";
 
-// const dynamodb = new AWS.DynamoDB.DocumentClient();
+const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 async function createDish(event, context) {
-  let recipe = await recipeScraper(
-    "https://www.allrecipes.com/recipe/229780/baked-denver-omelet/"
-  );
-  // const {
-  //   userId,
-  //   name,
-  //   servings,
-  //   directions,
-  //   ingredients,
-  //   image,
-  //   breakfast,
-  //   lunch,
-  //   dinner,
-  //   other,
-  // } = event.body;
-  // const { sub } = event.requestContext.authorizer;
+  const { userId, dishUrl } = event.body;
+  let recipe = await recipeScraper(dishUrl);
+  const now = new Date();
 
-  // const now = new Date();
+  const dish = {
+    userId,
+    name: recipe.name,
+    dishId: uuid(),
+    timestamp: now.toISOString(),
+    servings: recipe.servings,
+    ingredients: [],
+    directions: recipe.instructions,
+    image: recipe.image,
+    breakfast: "n",
+    lunch: "n",
+    dinner: "n",
+    other: "y",
+  };
 
-  // const dish = {
-  //   userId,
-  //   name,
-  //   dishId: uuid(),
-  //   timestamp: now.toISOString(),
-  //   servings,
-  //   ingredients,
-  //   directions,
-  //   image,
-  //   breakfast,
-  //   lunch,
-  //   dinner,
-  //   other,
-  // };
-
-  // try {
-  //   await dynamodb
-  //     .put({
-  //       TableName: process.env.RECIPES_TABLE_NAME,
-  //       Item: dish,
-  //     })
-  //     .promise();
-  // } catch (error) {
-  //   console.error(error);
-  //   throw new createError.InternalServerError(error);
-  // }
+  try {
+    await dynamodb
+      .put({
+        TableName: process.env.RECIPES_TABLE_NAME,
+        Item: dish,
+      })
+      .promise();
+  } catch (error) {
+    console.error(error);
+    throw new createError.InternalServerError(error);
+  }
 
   return {
     statusCode: 201,
-    body: JSON.stringify(recipe),
+    body: JSON.stringify(dish),
   };
 }
 
